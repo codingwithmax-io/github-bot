42b1e1752c00d2244a4bf4da20f71e4d201bc090
Max Carlquist <tenkaklet@gmail.com>
2024-02-21 22:41:03 +0100

    remade getRepoEvents
    main.js getRepoEvents

--- main.js
+++ main.js
@@ -13,28 +13,60 @@ const octokit = new Octokit({
     auth: process.env.TOKEN
 });
 
+/**
+ * Fetches the latest repository events from the GitHub API using the Octokit library.
+ */
 async function getRepoEvents() {
-    try {
-        const response = octokit.rest.activity.listPublicOrgEvents({
-            org: 'codingwithmax-io',
-        });
-    if(response.data) {
-        console.log('New Event: ', response.data[0].id);
-        console.log('New Event: ', response.data[0].type);
-        console.log('New Event: ', response.data[0].actor.login);
-        console.log('New Event: ', response.data[0].created_at);
-        console.log('New Event: ', response.data[0].repo.name);
-        console.log('New Event: ', response.data[0].payload);
-        events = response.data[0];
-    } else {
-        console.log('No new events');
+  try {
+    const response = octokit.rest.activity.listPublicOrgEvents({
+      org: 'codingwithmax-io',
+    });
+    // repo events are returned in an array
+    events = (await response).data[0];
+    /**
+     * If the response status is 200 (OK), log the following event properties:
+     * - Event type
+     * - Event actor
+     * - Created at
+     * - Repo
+     * - Payload
+     */
+    if ((await response).status === 200) {
+      console.log('Event Type: ', events.type);
+      console.log('Event Actor: ', events.actor.login);
+      console.log('Created At: ', events.created_at);
+      console.log('Repo: ', events.repo.name);
+      console.log('Payload: ', events.payload);
     }
-    } catch (error) {
-        console.log('ERROR: ', error);
+    /**
+     * If the response status is 304 (Not Modified), log the following event properties:
+     * - New event ID
+     * - New event type
+     * - New event actor
+     * - New event created at
+     * - New event repo
+     * - New event payload
+     */
+    else if ((await response).status === 304) {
+      console.log('New Event: ', events.id);
+      console.log('New Event: ', events.type);
+      console.log('New Event: ', events.actor.login);
+      console.log('New Event: ', events.created_at);
+      console.log('New Event: ', events.repo.name);
+      console.log('New Event: ', events.payload);
     }
+    /**
+     * If the response status is anything else, log the error.
+     */
+    else {
+      console.log('No new events');
+    }
+  } catch (error) {
+    console.log('ERROR: ', error);
+  }
 }
 
-setInterval(getRepoEvents, intervalTime);
+// setInterval(getRepoEvents, intervalTime);
 getRepoEvents();
 
 
