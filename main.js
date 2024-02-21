b1233e713972ccdd89fc850d2f221190e50e6a3c
Max Carlquist <tenkaklet@gmail.com>
2024-02-21 22:19:59 +0100

    Refactored code

--- main.js
+++ main.js
@@ -4,31 +4,42 @@ const port = process.env.PORT || 3000;
 const dotenv = require('dotenv');
 const path = require('path');
 const { Octokit } = require('@octokit/rest');
-const timeout = 10000;
-
+let events = undefined;
+const intervalTime = 5000; // 5 seconds
 dotenv.config();
 app.use(express.json());
 
 const octokit = new Octokit({
     auth: process.env.TOKEN
 });
-async function getRepoEvents() {
-    octokit.rest.activity.listPublicOrgEvents({
-        org: 'codingwithmax-io',
-    })
 
-        .then((data) => {
-            return data.data;
-        })
-        .catch((error) => {
-            console.log(error);
+async function getRepoEvents() {
+    try {
+        const response = octokit.rest.activity.listPublicOrgEvents({
+            org: 'codingwithmax-io',
         });
+    if(response.data) {
+        console.log('New Event: ', response.data[0].id);
+        console.log('New Event: ', response.data[0].type);
+        console.log('New Event: ', response.data[0].actor.login);
+        console.log('New Event: ', response.data[0].created_at);
+        console.log('New Event: ', response.data[0].repo.name);
+        console.log('New Event: ', response.data[0].payload);
+        events = response.data[0];
+    } else {
+        console.log('No new events');
+    }
+    } catch (error) {
+        console.log('ERROR: ', error);
+    }
 }
 
-setTimeout(async () => {
-    const events = await getRepoEvents();
-    console.log('THE EVENTS of the org is: ', events);
-}, timeout);
+setInterval(getRepoEvents, intervalTime);
+getRepoEvents();
+
+
+
+
 
 
 app.listen(port, () => {
